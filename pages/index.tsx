dcc6dae844775cab74ecc900056c9f461b52f7a9
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-10 19:28:17 -0600

    initial commit: configure next.js app, add login page

---
+++ pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <Head>
+        <title>Home - Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      Lets build Nextflix!
+      <main>
+
+      </main>
+    </div>
+  )
+}
+
+export default Home
