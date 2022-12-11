b3dfffbeed52e1f049c2685f9921fc2f16c5179a
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-10 20:01:19 -0600

    added tailwind utility clases to header, and ul with nav links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,25 @@
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+        <div className="flex items-center space-x-5 md:space-x-10 lg:space-x-14">
+            <img
+            src="https://rb.gy/ulxxee"
+            width={100}
+            height={100}
+            className="cursor-pointer object-contain"
+            />
+
+            <ul className="hidden space-x-4 md:flex">
+                <li className="headerLink">Home</li>
+                <li className="headerLink">TV Shows</li>
+                <li className="headerLink">Movies</li>
+                <li className="headerLink">New & Popular</li>
+                <li className="headerLink">My List</li>
+            </ul>
+        </div>
+
+        <div></div>
+    </header>
   )
 }
 
