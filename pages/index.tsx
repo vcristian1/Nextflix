4a40e371dedbd469e5853c4d68348549338c2172
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-10 19:32:54 -0600

    created header component in components directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
@@ -9,10 +10,7 @@ const Home: NextPage = () => {
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      Lets build Nextflix!
-      {/* Header */}
-      
+      <Header/>     
       <main>
         {/* Banner */}
         <section>
