8559a9a5d6df69bd5280836b0e0c4b31af43902d
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-11 21:08:38 -0600

    added API requests in request.ts for the movie api TMDB

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
 function Banner() {
   return (
-    <div>Banner</div>
+    <div>
+        <div>
+            {/* //Next.js component */}
+            
+        </div>
+    </div>
   )
 }
 
