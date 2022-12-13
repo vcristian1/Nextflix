b0306aa610fcffad1931da4bc7b8177ba0427826
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-12 20:38:17 -0600

    added tailwind utility class to style bg-color of more info button in Banner.tsx. Also imported the InformationCircleIcon from the react-heroicon

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,6 +3,7 @@ import { Movie } from '../typings';
 import { useEffect, useState} from 'react'
 import { baseUrl } from '../constants/movie'
 import { FaPlay } from 'react-icons/fa';
+import { InformationCircleIcon } from "@heroicons/react/solid";
 
 interface Props {
     netflixOriginals: Movie[]
@@ -38,7 +39,7 @@ function Banner({ netflixOriginals }: Props) {
 
             <div>
                 <button className="bannerButton bg-white text-black"><FaPlay className="h-4 w-4 text-black md:h-7 w-7"/>Play</button>
-                <button className="bannerButton">More Info</button>
+                <button className="bannerButton bg-[gray]/70">More Info <InformationCircleIcon className="h-5 w-5 md:h-8 md:w-8" /></button>
             </div>
 
         </div>
