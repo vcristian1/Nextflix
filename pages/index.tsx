a1abbb15a20411ccaf9ddf99291e093a8f9aa808
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-12 19:29:48 -0600

    added props and types in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,27 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import requests from '../utils/requests'
 
-const Home = ({ netflixOriginals }) => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+
+const Home = ({ 
+  netflixOriginals,
+  actionMovies,
+  comedyMovies,
+  documentaries,
+  horrorMovies,
+  romanceMovies,
+  topRated,
+  trendingNow,
+}: Props) => {
   console.log(netflixOriginals)
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
