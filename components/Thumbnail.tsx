7e043f7dc576c0040b85b7f4cfa7af69f488b071
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-13 21:27:57 -0600

    added logic to map over movies and within the function is the Thumbnail component so each Thumbnail is a different movie, also installed @next/codemod to resolve error rendering the thumbnails

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image"
+import { Movie } from "../typings"
 
+interface Props {
+    movie: Movie
+}
 
-function Thumbnail() {
+function Thumbnail({ movie }: Props) {
   return (
-    <div>Thumbnail</div>
+    <div className="relative h-28 min-w-[180px]">
+        <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        layout="fill"
+        alt="movie-cover"
+
+      />
+    </div>
   )
 }
 
