953f59b079911922e408db5540c01c05743e1db0
Cristian Vargas <cristian.v0223@gmail.com>
2022-12-15 19:32:11 -0600

    imported movies from typings to resolve deployment error

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,7 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import requests from '../utils/requests'
 import Row from '../components/Row'
-// import Movie from '../typings'
-
+import { Movie } from '../typings'
 interface Props {
   netflixOriginals: Movie[]
   trendingNow: Movie[]
@@ -28,7 +27,7 @@ const Home = ({
 }: Props) => {
   console.log(netflixOriginals)
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
