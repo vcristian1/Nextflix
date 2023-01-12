89f796a9cf28d9146ab561c6c5bc4711ef269017
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-12 12:43:11 -0600

    resolved type error

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
 import { useForm, SubmitHandler } from 'react-hook-form'
@@ -11,14 +11,21 @@ interface Inputs {
 
 function Login() {
   const [login, setLogin] = useState(false)
-  const {signIn, signUp} = useAuth()
+  const { signIn, signUp } = useAuth()
 
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit: SubmitHandler<Inputs> = async ({email, password}) => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    console.log(data)
     if (login) {
-      await signIn(email, password )
+      await signIn(data.email, data.password)
     } else {
-      await signUp(email, password)
+      await signUp(data.email, data.password)
     }
   }
 
@@ -33,39 +40,71 @@ function Login() {
         layout="fill"
         className="-z-10 !hidden opacity-60 sm:!inline"
         objectFit="cover"
-        alt="moviecover"
+        alt="movie-cover"
       />
-
       <img
         src="https://rb.gy/ulxxee"
         className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
         width={150}
         height={150}
-        alt="netflix-logo"
       />
-      
-      <form onSubmit={handleSubmit(onSubmit)} className='relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
-        <h1 className='text-4xl font-semibold'>Sign In</h1>
-        <div className='space-y-4'>
-          <label className="inline-block w-full" htmlFor="">
-            <input type="email" placeholder='Email' className='input' {...register('email', {required: true})} />
-            {errors.email && <p className='p-1 text-[13px] font-light text-orange-500'>Please enter a valid email</p>}
+
+      <form
+        className="relative mt-24 space-y-8 rounded bg-black/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <h1 className="text-4xl font-semibold">Sign In</h1>
+        <div className="space-y-4">
+          <label className="inline-block w-full">
+            <input
+              type="email"
+              placeholder="Email"
+              className={`input ${
+                errors.email && 'border-b-2 border-orange-500'
+              }`}
+              {...register('email', { required: true })}
+            />
+            {errors.email && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Please enter a valid email.
+              </p>
+            )}
           </label>
-          <label className="inline-block w-full" htmlFor="">
-            <input type="password" placeholder='Password' className='input' {...register('password', {required: true})}/>
-            {errors.password && <p className='p-1 text-[13px] font-light text-orange-500'>Please enter a valid password</p>}
+          <label className="inline-block w-full">
+            <input
+              type="password"
+              {...register('password', { required: true })}
+              placeholder="Password"
+              className={`input ${
+                errors.password && 'border-b-2 border-orange-500'
+              }`}
+            />
+            {errors.password && (
+              <p className="p-1 text-[13px] font-light  text-orange-500">
+                Your password must contain between 4 and 60 characters.
+              </p>
+            )}
           </label>
         </div>
-
-        <button className='w-full rounded bg-[#e50914] py-3 font-semibold' onClick={() => setLogin(true)}>Sign In</button>
-
-        <div className='text-[gray]'>
+        <button
+          className="w-full rounded bg-[#E50914] py-3 font-semibold"
+          onClick={() => setLogin(true)}
+          type="submit"
+        >
+          Sign In
+        </button>
+        <div className="text-[gray]">
           New to Netflix?{' '}
-          <button type="submit"className='text-white hover:underline' onClick={() => setLogin(false)}>Sign up now</button>
+          <button
+            className="cursor-pointer text-white hover:underline"
+            onClick={() => setLogin(false)}
+            type="submit"
+          >
+            Sign up now
+          </button>
         </div>
       </form>
     </div>
-    
   )
 }
 
