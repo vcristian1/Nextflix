a4da55ffb395eae796d8b91282f3efff6a4cb506
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-12 12:30:19 -0600

    attempt to resolve type error

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image'
 import { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import useAuth from '../hooks/useAuth'
 
 interface Inputs {
   email: string
@@ -10,12 +11,14 @@ interface Inputs {
 
 function Login() {
   const [login, setLogin] = useState(false)
+  const {signIn, signUp} = useAuth()
+
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = async ({email, password}) => {
+  const onSubmit: SubmitHandler<Inputs> = async ({email, password}) => {
     if (login) {
-      // await signIn(email, password )
+      await signIn(email, password )
     } else {
-      // await signUp(email, password)
+      await signUp(email, password)
     }
   }
 
