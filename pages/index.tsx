46e8648c559528c4424487f3cb5fe8dcf180bc65
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-13 15:18:56 -0600

    added logic in index.tsx for the showModal and if the showModal is true, to then adjust the styling of the container holding the rows of movies behind the modal so you cannot scroll whats behind the modal and only the modal itself

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,8 +38,9 @@ const Home = ({
   if (loading) return null
 
   return (
+    // If show modal is true, overflow is hidden to prevent scrolling when the modal is open
     <div
-      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]`}
+      className={`relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh] ${showModal &&  '!h-screen overflow-hidden'}`}
     >
       <Head>
         <title>
