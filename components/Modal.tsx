5875cd060ed19d338e008636505d3149b6acc95a
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-12 19:27:58 -0600

    added onClick from banner to thumbnail so when you click the thumbnail it will also open the modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,9 +2,31 @@ import MuiModal from "@mui/material/Modal"
 import { modalState } from "../atoms/modalAtom"
 import { useRecoilState } from "recoil"
 import { XIcon } from "@heroicons/react/solid"
+import { useEffect, useState } from "react"
+import { Movie } from "../typings"
 
 function Modal() {
   const [showModal, setShowModal] = useRecoilState(modalState)
+  const [movie, setMovie] = useState<Movie | null>(null)
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    if (!movie) return
+
+    async function fetchMovie() {
+        const data = await fetch(`https://api.themoviedb.org/3/${
+            movie?.media_type === 'tv' ? 'tv' : 'movie'
+          }/${movie?.id}?api_key=${
+            process.env.NEXT_PUBLIC_API_KEY
+          }&language=en-US&append_to_response=videos`
+          ).then((response) => response.json())
+          setData(data)
+    }
+    fetchMovie()
+  },[movie])
+
+  console.log(data)
+
 
   const handleClose = () => {
     setShowModal(false)
@@ -17,6 +39,10 @@ function Modal() {
             className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
                 <XIcon className="h-6 w-6" />
             </button>
+            {/* fetches real time videos */}
+            <div>
+
+            </div>
         </>
     </MuiModal>
   )
