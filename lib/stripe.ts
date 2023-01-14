eea86ec3921ca2f48b0664b5abf9033ddcd39126
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-14 10:51:49 -0600

    configuring stripe: added loadCheckout function which is an async function that uses createCheckoutSession to create a checkoutSession for the user

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -11,4 +11,12 @@ const payments = getStripePayments(app, {
     customersCollection: 'customers'
 })
 
-
+const loadCheckout = async (priceId: string) => {
+    await createCheckoutSession(payments, {
+        price: priceId,
+        //If you want a custom url just add the url in a string
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+        //The snapshot url is the link to the checkout sesssion, a stripe payment portal, which is given to us by Stripe. 
+    }).then((snapshot) => window.location.assign(snapshot.url)).catch((error) => console.log(error.message))
+}
