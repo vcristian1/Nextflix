c34dcecbf245defa9d2dee44b451c91bbd27dcdc
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-14 10:45:52 -0600

    configuring stripe: added payments variable and set equal to getStripePaymentsmethod to set up payments

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -2,5 +2,13 @@ import {
     createCheckoutSession,
     getStripePayments,
   } from '@stripe/firestore-stripe-payments'
-  import { getFunctions, httpsCallable } from '@firebase/functions'
-  import app from '../firebase'
+import { getFunctions, httpsCallable } from '@firebase/functions'
+import app from '../firebase'
+
+const payments = getStripePayments(app, {
+    //this is the name of the collection, if the name of collection is plans then change to plans
+    productsCollection: 'products',
+    customersCollection: 'customers'
+})
+
+
