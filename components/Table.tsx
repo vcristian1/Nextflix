762412b26eecefda2d8707cc83935be16df8baa6
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-14 16:47:39 -0600

    added custom classes for styling for the table row, table data

--- components/Table.tsx
+++ components/Table.tsx
@@ -7,12 +7,28 @@ interface Props {
 function Table({ products }) {
   return (
     <table>
-        <tbody>
-            <tr>
-                <td>Monthly price</td>
+        <tbody className="divide-y divide-[gray]">
+            <tr className="tableRow">
+                <td className="tableDataTitle">Price</td>
                 {products.map((product) => (
                     <td className="tableDataFeature" key={product.id}>
                         ${product.prices[0].unit_amount! / 100}
+                    </td>
+                ))}
+            </tr>
+            <tr className="tableRow">
+                <td className="tableDataTitle">Quality</td>
+                {products.map((product) => (
+                    <td className="tableDataFeature" key={product.id}>
+                        {product.metadata.videoQuality}
+                    </td>
+                ))}
+            </tr>
+            <tr className="tableRow">
+                <td className="tableDataTitle">Resolution</td>
+                {products.map((product) => (
+                    <td className="tableDataFeature" key={product.id}>
+                        {product.metadata.resolution}
                     </td>
                 ))}
             </tr>
