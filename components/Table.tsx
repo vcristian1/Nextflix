82ede817988d1ad20e0f746d7b0e40c3c4e10b24
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-16 13:28:19 -0600

    resolved merge conflict

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,24 +1,12 @@
 import { CheckIcon } from '@heroicons/react/outline'
-<<<<<<< HEAD
 
 
 function Table() {
-=======
-import { Product } from '@stripe/firestore-stripe-payments'
-
-interface Props {
-  products: Product[]
-  selectedPlan: Product | null
-}
-
-function Table({ products, selectedPlan }: Props) {
->>>>>>> 28a49f2fc94f00236a2724624bef1646efca4ce3
   return (
     <table>
       <tbody className="divide-y divide-[gray]">
         <tr className="tableRow">
           <td className="tableDataTitle">Monthly price</td>
-<<<<<<< HEAD
           
         </tr>
         <tr className="tableRow">
@@ -28,96 +16,18 @@ function Table({ products, selectedPlan }: Props) {
         <tr className="tableRow">
           <td className="tableDataTitle">Resolution</td>
           
-=======
-          {products.map((product) => (
-            <td
-              className={`tableDataFeature ${
-                selectedPlan?.id === product.id
-                  ? 'text-[#E50914]'
-                  : 'text-[gray]'
-              }`}
-              key={product.id}
-            >
-              ${product.prices[0].unit_amount! / 100}
-            </td>
-          ))}
-        </tr>
-        <tr className="tableRow">
-          <td className="tableDataTitle">Video quality</td>
-          {products.map((product) => (
-            <td
-              className={`tableDataFeature ${
-                selectedPlan?.id === product.id
-                  ? 'text-[#E50914]'
-                  : 'text-[gray]'
-              }`}
-              key={product.id}
-            >
-              {product.metadata.videoQuality}
-            </td>
-          ))}
-        </tr>
-        <tr className="tableRow">
-          <td className="tableDataTitle">Resolution</td>
-          {products.map((product) => (
-            <td
-              className={`tableDataFeature ${
-                selectedPlan?.id === product.id
-                  ? 'text-[#E50914]'
-                  : 'text-[gray]'
-              }`}
-              key={product.id}
-            >
-              {product.metadata.resolution}
-            </td>
-          ))}
->>>>>>> 28a49f2fc94f00236a2724624bef1646efca4ce3
         </tr>
         <tr className="tableRow">
           <td className="tableDataTitle">
             Watch on your TV, computer, mobile phone and tablet
           </td>
-<<<<<<< HEAD
           
-=======
-          {products.map((product) => (
-            <td
-              className={`tableDataFeature ${
-                selectedPlan?.id === product.id
-                  ? 'text-[#E50914]'
-                  : 'text-[gray]'
-              }`}
-              key={product.id}
-            >
-              {product.metadata.portability === 'true' && (
-                <CheckIcon className="inline-block h-8 w-8" />
-              )}
-            </td>
-          ))}
->>>>>>> 28a49f2fc94f00236a2724624bef1646efca4ce3
         </tr>
         <tr className="tableRow">
           <td className="tableDataTitle">
             Downloads
           </td>
-<<<<<<< HEAD
           
-=======
-          {products.map((product) => (
-            <td
-              className={`tableDataFeature ${
-                selectedPlan?.id === product.id
-                  ? 'text-[#E50914]'
-                  : 'text-[gray]'
-              }`}
-              key={product.id}
-            >
-              {product.metadata.downloads === 'true' && (
-                <CheckIcon className="inline-block h-8 w-8" />
-              )}
-            </td>
-          ))}
->>>>>>> 28a49f2fc94f00236a2724624bef1646efca4ce3
         </tr>
       </tbody>
     </table>
