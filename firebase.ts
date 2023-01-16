8d79afecf9f70cd7cd0ca7c35b9f833b992301f2
Cristian Vargas <cristian.v0223@gmail.com>
2023-01-15 19:08:55 -0600

    debugging error

--- firebase.ts
+++ firebase.ts
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
-import { getApp, getApps, initializeApp } from 'firebase/app'
+import { initializeApp, getApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -16,13 +17,13 @@ import { getAuth } from 'firebase/auth'
 // };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCKeYR7avRbH87LWPMCyQybsiaWjoQMTUw",
-    authDomain: "nextflix-js.firebaseapp.com",
-    projectId: "nextflix-js",
-    storageBucket: "nextflix-js.appspot.com",
-    messagingSenderId: "527167388314",
-    appId: "1:527167388314:web:9a79e45a8b1f1091d53d23"
-  };
+  apiKey: "AIzaSyDNWAU3B0fzymE_c8n-pIzFvcuoquLarjg",
+  authDomain: "nextflix-53016.firebaseapp.com",
+  projectId: "nextflix-53016",
+  storageBucket: "nextflix-53016.appspot.com",
+  messagingSenderId: "774759801474",
+  appId: "1:774759801474:web:e8ba5a01ae475cb3408a9b"
+};
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
